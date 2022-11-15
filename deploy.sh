c6b6de1532c6e7329556510a40d0f07615a08a51
Vikram Nagarkar <vikram.nagarkar@yahoo.com>
2022-11-15 17:49:26 +0000

    Updated

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 echo "Creating namespace"
 kubectl apply -f ./install/00-kateyes-ns.yaml
 echo "Creating Ingress Controller"
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
 helm upgrade -n kateyes kateyes-nginx ingress-nginx/ingress-nginx --version 4.2.1 --install -f ./install/ingress-controller/values.yaml
 echo "Installing Kateyes"
--- install/ingress-controller/install.sh
+++ install/ingress-controller/install.sh
@@ -1,1 +1,1 @@
-helm upgrade -n kateyes kateyes-nginx ingress-nginx/ingress-nginx --version $INGRESS_VERSION --install -f values.yaml
+helm upgrade -n kateyes kateyes-nginx ingress-nginx/ingress-nginx --version $INGRESS_VERSION --install -f values.yamlinstall ingress 
